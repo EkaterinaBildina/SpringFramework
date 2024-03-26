4abb060bb6441640b61fab39281b62342e2dfa61
EkaterinaBildina <kukresch@gmail.com>
2024-03-26 12:29:39 +0700

    Bildina hw03 to do

--- Seminar3/sem3HomeTask/src/main/java/com/example/sem3HomeTask/services/RegistrationService.java
+++ Seminar3/sem3HomeTask/src/main/java/com/example/sem3HomeTask/services/RegistrationService.java
@@ -1,5 +1,6 @@
 package com.example.sem3HomeTask.services;
 
+import com.example.sem3HomeTask.domain.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +16,22 @@ public class RegistrationService {
     private DataProcessingService dataProcessingService;
 
     //Поля UserService, NotificationService
+    private final UserService userService;
+    private final NotificationService notificationService;
+
+    //Конструктор
+    public RegistrationService(UserService userService, NotificationService notificationService) {
+        this.userService = userService;
+        this.notificationService = notificationService;
+    }
+
 
     //Метод processRegistration
+    public void processRegistration(String userName, int age, String email) {
+       //User user = new User(userName, age, email);
+       // userService.save(user);
+        userService.createUser(userName, age, email);
+        notificationService.sendNotification("Пользователь " + userName + " успешно зарегистрирован!");
+    }
+
 }
